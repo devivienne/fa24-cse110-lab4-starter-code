2f3a88cbc04a78761f163f92518d5d94967a92de
Zere-m <146875320+Zere-m@users.noreply.github.com>
2024-11-04 02:32:26 -0800

    adding up to exercises

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/not gonna be there - test/i);
-  expect(linkElement).toBeInTheDocument();
-});
