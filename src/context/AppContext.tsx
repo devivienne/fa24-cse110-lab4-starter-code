64d4252312b0417157d5b140c3f245776e375eaa
devvbella <ava0712@naver.com>
2024-11-03 23:19:12 -0800

    exercise 3-1 done

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -6,20 +6,25 @@ import { Expense } from "../types/types";
 interface AppContextType {
   expenses: Expense[];
   setExpenses: React.Dispatch<React.SetStateAction<Expense[]>>;
+  budget: number;
+  setBudget: React.Dispatch<React.SetStateAction<number>>;
 }
 
 const initialState: AppContextType = {
   expenses: [],
   setExpenses: () => {},
+  budget: 1000, // Initial budget value
+  setBudget: () => {},
 };
 
 export const AppContext = createContext<AppContextType>(initialState);
 
 export const AppProvider = ({ children }: { children: ReactNode }) => {
   const [expenses, setExpenses] = useState<Expense[]>(initialState.expenses);
+  const [budget, setBudget] = useState<number>(initialState.budget);
 
   return (
-    <AppContext.Provider value={{ expenses, setExpenses }}>
+    <AppContext.Provider value={{ expenses, setExpenses, budget, setBudget }}>
       {children}
     </AppContext.Provider>
   );
